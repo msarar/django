547cd00ee49ddbcb210c3dfb1460dd4d4dd1ab52
unknown <galaxyraad@gmail.com>
2020-05-05 18:55:46 -0700

    added a blog app with class based views: upto vid 39

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -20,6 +20,7 @@ from pages.views import home_view, contact_view, about_view, social_view
 
 urlpatterns = [
     path('products/', include('products.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('contact/', contact_view, name='contact'),
